e83685f8fd7c8f8460c4489a9a51296be58c131c
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-26 17:06:25 +0000

    METAMAC-1909: [API] La api externa debe consumir api externa de SRM http://jira.arte-consultores.com/browse/METAMAC-1909
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-registry/trunk/metamac-registry@51607 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-registry-web/src/main/java/org/siemac/metamac/srm/registry/ApplicationStartup.java
+++ metamac-registry-web/src/main/java/org/siemac/metamac/srm/registry/ApplicationStartup.java
@@ -7,8 +7,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+import org.siemac.metamac.sdmx.data.rest.external.conf.DataConfigurationConstants;
 
-import com.arte.statistic.sdmx.srm.rest.external.conf.SrmConfigurationConstants;
+import com.arte.statistic.sdmx.srm.core.constants.SdmxSrmConfigurationConstants;
 
 public class ApplicationStartup implements ServletContextListener {
 
@@ -29,23 +30,44 @@ public class ApplicationStartup implements ServletContextListener {
 
     private void checkConfiguration() {
         LOG.info("**********************************************************");
-        LOG.info("[metamac-registry-web] Checking application configuration");
+        LOG.info("[metamac-registry-web] Checking application configuration: Datasource SRM");
         LOG.info("**********************************************************");
 
         // Datasource
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_DRIVER_NAME);
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_URL);
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_USERNAME);
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_PASSWORD);
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_DIALECT);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.DB_DRIVER_NAME);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.DB_URL);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.DB_USERNAME);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.DB_PASSWORD);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.DB_DIALECT);
+
+        LOG.info("**********************************************************");
+        LOG.info("[metamac-registry-web] Application configuration checked: Datasource STATISTICAL_RESOURCES AND STATISTICAL_RESOURCES_REPOSITORY");
+        LOG.info("**********************************************************");
+
+        // Datasource
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_URL);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_USERNAME);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_PASSWORD);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_DIALECT);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_DRIVER_NAME);
+
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_REPOSITORY_URL);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_REPOSITORY_USERNAME);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_REPOSITORY_PASSWORD);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_REPOSITORY_DIALECT);
+        configurationService.checkRequiredProperty(DataConfigurationConstants.DB_REPOSITORY_DRIVER_NAME);
+
+        LOG.info("**********************************************************");
+        LOG.info("[metamac-registry-web] Checking application configuration: APIS");
+        LOG.info("**********************************************************");
 
         // Api
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.METAMAC_ORGANISATION_URN);
-        configurationService.checkRequiredProperty(SrmConfigurationConstants.METAMAC_ORGANISATION);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.METAMAC_ORGANISATION_URN);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.METAMAC_ORGANISATION);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.ENDPOINT_SDMX_REGISTRY_EXTERNAL_API);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.ENDPOINT_SDMX_SRM_EXTERNAL_API);
+        configurationService.checkRequiredProperty(SdmxSrmConfigurationConstants.ENDPOINT_SDMX_STATISTICAL_RESOURCES_EXTERNAL_API);
 
-        LOG.info("**********************************************************");
-        LOG.info("[metamac-registry-web] Application configuration checked");
-        LOG.info("**********************************************************");
     }
 
     @Override
