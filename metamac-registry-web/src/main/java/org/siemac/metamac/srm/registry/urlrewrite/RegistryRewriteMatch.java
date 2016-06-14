e564e58f18222a74cdd46b0ae95ad87f3a7809af
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-06-14 13:09:48 +0000

    METAMAC-2460 - Revisar APIs: redireccionamiento y acceso a la documentación
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-registry/trunk/metamac-registry@55481 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-registry-web/src/main/java/org/siemac/metamac/srm/registry/urlrewrite/RegistryRewriteMatch.java
+++ metamac-registry-web/src/main/java/org/siemac/metamac/srm/registry/urlrewrite/RegistryRewriteMatch.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.util.ApplicationContextProvider;
@@ -39,7 +40,11 @@ public class RegistryRewriteMatch extends RewriteMatch {
                 String location = buildTargetLocation(SDMX_API_VERSION, requestPathAfterVersion, queryString);
                 response.sendRedirect(location);
                 return true;
-            } else if (requestPathAfterVersion.startsWith("/data")) {
+            } else if (StringUtils.isNotBlank(queryString) && queryString.contains("_wadl")) {
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("/downloadWadl?path=/apis/registry/v2.1?_wadl");
+                requestDispatcher.forward(request, response);
+                return true;
+            } else if (requestPathAfterVersion != null && requestPathAfterVersion.startsWith("/data")) {
                 RequestDispatcher requestDispatcher = request.getRequestDispatcher("/data/registry/" + requestApiVersion + requestPathAfterVersion);
                 requestDispatcher.forward(request, response);
                 return true;
