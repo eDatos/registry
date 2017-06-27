f3956c21b5d969b572ba7767c0dbe33849a6f760
acidcar <acidcar@arte-consultores.com>
2017-06-27 14:28:20 +0100

    METAMAC-2624 - Crear tarea para despliegue de todo metamac

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -5,6 +5,12 @@ TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
+RESTART=1
+
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
+
 
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp metamac-registry-web/target/sdmx-registry-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/sdmx-registry.war
@@ -13,8 +19,10 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
 
-    sudo service metamac01 stop
-    checkPROC "metamac"
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+        checkPROC "metamac"
+    fi
 
 
     ###
@@ -31,10 +39,12 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/sdmx-registry/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/sdmx-registry/$LOGBACK_RELATIVE_PATH_FILE
 
-    sudo chown -R metamac.metamac /servers/metamac
-    sudo service metamac01 start
-    
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
+
     # checkURL "http://estadisticas.arte-consultores.com/registry/v2.1?_wadl" "metamac01"
     echo "Finished deploy"
-    
+
 EOF
